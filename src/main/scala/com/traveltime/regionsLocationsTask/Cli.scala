202ce5477d40dc00d509e1524197a3c60b8fa1e4
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-07-31 13:09:10 +0300

    Added running with files passed as arguments

--- src/main/scala/com/traveltime/regionsLocationsTask/Cli.scala
+++ src/main/scala/com/traveltime/regionsLocationsTask/Cli.scala
@@ -1,3 +1,19 @@
 package com.traveltime.regionsLocationsTask
 
-object Cli {}
+import cats.implicits.catsSyntaxTuple3Semigroupal
+import com.monovore.decline._
+
+object Cli {
+  val locationsInputFile: Opts[String] =
+    Opts.option[String]("locations", help = "Path to locations JSON file")
+  val regionsInputFile: Opts[String] =
+    Opts.option[String]("regions", help = "Path to regions JSON file")
+  val outputFile: Opts[String] =
+    Opts.option[String]("output", help = "Path to output JSON file")
+
+  val command: Opts[(String, String, String)] = Opts.subcommand(
+    Command(name = "match", header = "Matches locations to regions")(
+      (locationsInputFile, regionsInputFile, outputFile).tupled
+    )
+  )
+}
--- src/main/scala/com/traveltime/regionsLocationsTask/Main.scala
+++ src/main/scala/com/traveltime/regionsLocationsTask/Main.scala
@@ -1,5 +1,7 @@
 package com.traveltime.regionsLocationsTask
 
+import cats.implicits._
+import com.monovore.decline.{CommandApp, Opts}
 import io.circe.Decoder
 import io.circe.generic.auto._
 import io.circe.parser._
@@ -10,78 +12,91 @@ import scala.util.Using
 import scala.io.Source
 import java.nio.charset.StandardCharsets
 import com.traveltime.regionsLocationsTask.Codecs._
+import com.traveltime.regionsLocationsTask.Cli._
 
-object Main extends App {
-  def parseAndDecode[T: Decoder](
-      jsonFilePath: String
-  ): Either[Throwable, List[T]] = {
-    val tryJsonString = Using(Source.fromFile(jsonFilePath))(_.mkString)
+object Main
+    extends CommandApp(
+      name = "regions-locations",
+      header = "Matches locations to regions",
+      main = {
+        Cli.command.map { case (locationsFile, regionsFile, outputFile) =>
+          def parseAndDecode[T: Decoder](
+              jsonFilePath: String
+          ): Either[Throwable, List[T]] = {
+            val tryJsonString = Using(Source.fromFile(jsonFilePath))(_.mkString)
 
-    tryJsonString.toEither.flatMap { jsonString =>
-      parse(jsonString).flatMap(_.as[List[T]])
-    }
-  }
+            tryJsonString.toEither.flatMap { jsonString =>
+              parse(jsonString).flatMap(_.as[List[T]])
+            }
+          }
 
-  def pointInPolygon(point: Coordinate, polygon: List[Coordinate]): Boolean = {
-    val x = point.x
-    val y = point.y
-    var isInside = false
-    var j = polygon.size - 1
+          def pointInPolygon(
+              point: Coordinate,
+              polygon: List[Coordinate]
+          ): Boolean = {
+            val x = point.x
+            val y = point.y
+            var isInside = false
+            var j = polygon.size - 1
 
-    for (i <- polygon.indices) {
-      val xi = polygon(i).x
-      val yi = polygon(i).y
-      val xj = polygon(j).x
-      val yj = polygon(j).y
+            for (i <- polygon.indices) {
+              val xi = polygon(i).x
+              val yi = polygon(i).y
+              val xj = polygon(j).x
+              val yj = polygon(j).y
 
-      val intersect = ((yi > y) != (yj > y)) &&
-        (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
+              val intersect = ((yi > y) != (yj > y)) &&
+                (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
 
-      if (intersect) isInside = !isInside
-      j = i
-    }
+              if (intersect) isInside = !isInside
+              j = i
+            }
 
-    isInside
-  }
+            isInside
+          }
 
-  // Parse and decode JSON for locations
-  val locationsResult = parseAndDecode[Location]("Input/locations.json")
+          // Parse and decode JSON for locations
+          val locationsResult = parseAndDecode[Location](locationsFile)
 
-  // Parse and decode JSON for regions
-  val regionsResult = parseAndDecode[Region]("Input/regions.json")
+          // Parse and decode JSON for regions
+          val regionsResult = parseAndDecode[Region](regionsFile)
 
-  val regionWithLocations = regionsResult.flatMap { regions =>
-    locationsResult.map { locations =>
-      regions.map { region =>
-        val matchedLocations = locations.filter(location =>
-          region.coordinates.exists(polygon =>
-            pointInPolygon(location.coordinates, polygon)
-          )
-        )
-        RegionWithLocations(region.name, matchedLocations.map(_.name))
+          val regionWithLocations = regionsResult.flatMap { regions =>
+            locationsResult.map { locations =>
+              regions.map { region =>
+                val matchedLocations = locations.filter(location =>
+                  region.coordinates.exists(polygon =>
+                    pointInPolygon(location.coordinates, polygon)
+                  )
+                )
+                RegionWithLocations(region.name, matchedLocations.map(_.name))
+              }
+            }
+          }
+
+          regionWithLocations match {
+            case Left(failure) =>
+              println(s"Failed to process: ${failure.getMessage}")
+            case Right(results) =>
+              results.foreach(result =>
+                println(
+                  s"Region: ${result.region}, Matched Locations: ${result.matched_locations}"
+                )
+              )
+          }
+
+          regionWithLocations match {
+            case Right(regions) =>
+              val jsonString =
+                regions.asJson.spaces2 // Convert the results to JSON
+              Files.write(
+                Paths.get(outputFile),
+                jsonString.getBytes(StandardCharsets.UTF_8)
+              ) // Write to a file
+              ()
+            case Left(error) =>
+              println(s"An error occurred: ${error.getMessage}")
+          }
+        }
       }
-    }
-  }
-
-  regionWithLocations match {
-    case Left(failure) =>
-      println(s"Failed to process: ${failure.getMessage}")
-    case Right(results) =>
-      results.foreach(result =>
-        println(
-          s"Region: ${result.region}, Matched Locations: ${result.matched_locations}"
-        )
-      )
-  }
-
-  regionWithLocations match {
-    case Right(regions) =>
-      val jsonString = regions.asJson.spaces2 // Convert the results to JSON
-      Files.write(
-        Paths.get("Output/results.json"),
-        jsonString.getBytes(StandardCharsets.UTF_8)
-      ) // Write to a file
-    case Left(error) =>
-      println(s"An error occurred: ${error.getMessage}")
-  }
-}
+    )
