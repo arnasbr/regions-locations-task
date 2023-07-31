27cbe297226af0a46abe9b2e98ba25f90d66899d
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-07-31 11:03:11 +0300

    Made logic to find locations in regions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,6 +9,8 @@ case class Coordinate(x: Double, y: Double)
 case class Region(name: String, coordinates: List[List[Coordinate]])
 case class Location(name: String, coordinates: Coordinate)
 
+case class RegionWithLocations(region: String, matched_locations: List[String])
+
 object Main extends App {
 
   // Custom decoders
@@ -31,9 +33,55 @@ object Main extends App {
     }
   }
 
+  def pointInPolygon(point: Coordinate, polygon: List[Coordinate]): Boolean = {
+    val x = point.x
+    val y = point.y
+    var isInside = false
+    var j = polygon.size - 1
+
+    for (i <- polygon.indices) {
+      val xi = polygon(i).x
+      val yi = polygon(i).y
+      val xj = polygon(j).x
+      val yj = polygon(j).y
+
+      val intersect = ((yi > y) != (yj > y)) &&
+        (x < (xj - xi) * (y - yi) / (yj - yi) + xi)
+
+      if (intersect) isInside = !isInside
+      j = i
+    }
+
+    isInside
+  }
+
   // Parse and decode JSON for locations
   val locationsResult = parseAndDecode[Location]("Input/locations.json")
 
   // Parse and decode JSON for regions
   val regionsResult = parseAndDecode[Region]("Input/regions.json")
+
+  val regionWithLocations = regionsResult.flatMap { regions =>
+    locationsResult.map { locations =>
+      regions.map { region =>
+        val matchedLocations = locations.filter(location =>
+          region.coordinates.exists(polygon =>
+            pointInPolygon(location.coordinates, polygon)
+          )
+        )
+        RegionWithLocations(region.name, matchedLocations.map(_.name))
+      }
+    }
+  }
+
+  regionWithLocations match {
+    case Left(failure) =>
+      println(s"Failed to process: ${failure.getMessage}")
+    case Right(results) =>
+      results.foreach(result =>
+        println(
+          s"Region: ${result.region}, Matched Locations: ${result.matched_locations}"
+        )
+      )
+  }
 }
