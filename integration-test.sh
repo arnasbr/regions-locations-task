a50bda60acfdc660b619c8b7b756d05df3fa58ed
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-08-01 11:51:06 +0300

    Created integration-test.sh

---
+++ integration-test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Execute sbt command
+sbt "run match --locations Input/test1/locations.json --regions Input/test1/regions.json --output Output/results.json"
+
+# Compare the output to the expected output
+diff Output/results.json Output/expectedResults.json
+
+# Check the result of diff (0 means no differences, 1 means differences)
+if [ $? -eq 0 ]
+then
+    echo "The test passed successfully, the output matches the expected results."
+else
+    echo "The test failed, the output does not match the expected results."
+fi
