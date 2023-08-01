407ede14f394665d3b8d3656577da22853ed310d
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-08-01 12:43:34 +0300

    Created resources folder, updated workflow tests

--- integration-test.sh
+++ integration-test.sh
@@ -4,7 +4,7 @@
 sbt "run match --locations Input/test1/locations.json --regions Input/test1/regions.json --output Output/results.json"
 
 # Compare the output to the expected output
-diff Output/results.json Output/expectedResults.json
+diff src/main/resources/output/results.json src/main/resources/expectedResults.json
 
 # Check the result of diff (0 means no differences, 1 means differences)
 if [ $? -eq 0 ]
