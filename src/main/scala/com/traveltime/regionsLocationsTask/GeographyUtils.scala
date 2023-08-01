a21d016e0afdfee0f015ce7321f93e96fa71acc6
Arnas Bradauskas <arnas.bradauskas@traveltime.com>
2023-08-01 10:59:42 +0300

    Reworked daunting pointInPolygon method

--- src/main/scala/com/traveltime/regionsLocationsTask/GeographyUtils.scala
+++ src/main/scala/com/traveltime/regionsLocationsTask/GeographyUtils.scala
@@ -2,30 +2,21 @@ package com.traveltime.regionsLocationsTask
 import Models._
 
 object GeographyUtils {
-  def pointInPolygon(
-      point: Coordinate,
-      polygon: List[Coordinate]
-  ): Boolean = {
-    val windingNumber: Int = polygon
-      .sliding(2)
-      .collect {
-        case a :: b :: Nil
-            if a.y <= point.y && b.y > point.y && isLeft(
-              a,
-              b,
-              point
-            ) > 0 =>
-          1
-        case a :: b :: Nil
-            if a.y > point.y && b.y <= point.y && isLeft(
-              a,
-              b,
-              point
-            ) < 0 =>
-          -1
-        case _ => 0
-      }
-      .sum
+  def pointInPolygon(point: Coordinate, polygon: List[Coordinate]): Boolean = {
+    // Create pairs of consecutive points from the polygon
+    val pointPairs = polygon.sliding(2).toList
+
+    // Calculate winding numbers
+    val windingNumber = pointPairs.map {
+      case List(a, b)
+          if a.y <= point.y && b.y > point.y && isLeft(a, b, point) > 0 =>
+        1
+      case List(a, b)
+          if a.y > point.y && b.y <= point.y && isLeft(a, b, point) < 0 =>
+        -1
+      case _ => 0
+    }.sum
+
     windingNumber != 0
   }
 
